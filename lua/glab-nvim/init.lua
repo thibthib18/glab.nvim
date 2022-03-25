9d6db601c2c58e9d4f4a01f173d1cb38e56cc9c9
thib <thibault@seervision.com>
2022-03-25 11:16:42 +0000

    add init.lua

---
+++ lua/glab-nvim/init.lua
@@ -0,0 +1,24 @@
+local config = require("glab-nvim.config")
+local api = require("glab-nvim.api.gitlab.api")
+local pickers = require "glab-nvim.telescope.pickers"
+
+local M = {}
+
+function M.pick_merge_requests()
+    local function on_result(output)
+        pickers.merge_request_picker(output)
+    end
+    local project = config.get_config().project
+    print(vim.inspect(project))
+    api.get_merge_requests(project.owner, project.name, on_result)
+end
+
+function M.setup(user_config)
+    for key, value in pairs(user_config) do
+        if value ~= nil then
+            config.config[key] = value
+        end
+    end
+end
+
+return M
