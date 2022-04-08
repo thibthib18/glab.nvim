38bfdf6005e6aa640f09e9bfa4ecf281d84febd8
thib <thibault@seervision.com>
2022-04-08 16:23:00 +0000

    add mr_view render function

--- lua/glab-nvim/view/render.lua
+++ lua/glab-nvim/view/render.lua
@@ -8,4 +8,13 @@ function M.mr_preview(bufnr, merge_request)
     writers.write_state(bufnr, merge_request.state:upper(), merge_request.iid)
 end
 
+function M.mr_view(bufnr, merge_request)
+    writers.write_title(bufnr, merge_request.title, 1)
+    writers.write_mr_details(bufnr, merge_request)
+    writers.write_mr_body(bufnr, merge_request)
+    writers.write_state(bufnr, merge_request.state:upper(), merge_request.iid)
+    local line = vim.api.nvim_buf_line_count(bufnr)
+    writers.write_discussions(bufnr, merge_request.discussions.nodes, line)
+end
+
 return M
