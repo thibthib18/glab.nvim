9b26fea19d23b3567c1baf112e7801e11acdc097
thib <thibault@seervision.com>
2023-03-23 14:47:00 +0400

    use get_project

--- lua/glab-nvim/telescope/previewers.lua
+++ lua/glab-nvim/telescope/previewers.lua
@@ -7,24 +7,24 @@ local render = require("glab-nvim.view.render")
 local M = {}
 
 function M.mr_preview()
-    return previewers.new_buffer_previewer {
-        get_buffer_by_name = function(_, entry)
-            return entry.name
-        end,
-        define_preview = function(self, entry)
-            local bufnr = self.state.bufnr
-            if self.state.bufname ~= entry.name or vim.api.nvim_buf_line_count(bufnr) == 1 then
-                api.get_merge_request(
-                    config.project.owner,
-                    config.project.name,
-                    entry.merge_request.iid,
-                    function(merge_request)
-                        render.mr_preview(bufnr, merge_request)
-                    end
-                )
-            end
-        end
-    }
+  return previewers.new_buffer_previewer {
+    get_buffer_by_name = function(_, entry)
+      return entry.name
+    end,
+    define_preview = function(self, entry)
+      local bufnr = self.state.bufnr
+      if self.state.bufname ~= entry.name or vim.api.nvim_buf_line_count(bufnr) == 1 then
+        api.get_merge_request(
+          config.get_project().owner,
+          config.project.name,
+          entry.merge_request.iid,
+          function(merge_request)
+            render.mr_preview(bufnr, merge_request)
+          end
+        )
+      end
+    end
+  }
 end
 
 return M
