a598ca069b3c45009c9e187ba694ddfcfea446b1
thib <thibault@seervision.com>
2023-03-23 14:45:41 +0400

    get project directly from remote url

--- lua/glab-nvim/config.lua
+++ lua/glab-nvim/config.lua
@@ -1,16 +1,51 @@
 local M = {}
 
 M.config = {
-    provider_hostname = "",
-    username = "thibthib",
-    project = {
-        owner = "thibthib",
-        name = "test-project"
-    }
+  provider_hostname = "",
+  username = "thibthib",
 }
 
 function M.get_config()
-    return M.config
+  return M.config
+end
+
+local function split(text, delimiter)
+  local chunks = {}
+  for chunk in string.gmatch(text, "([^" .. delimiter .. "]+)") do
+    table.insert(chunks, chunk)
+  end
+  return chunks
+end
+
+local function get_project_from_url(url)
+  local command = 'git config --get remote.origin.url'
+  local url = vim.fn.system(command)
+  local isHttp = string.match(url, 'https://') ~= nil
+  if isHttp then
+    local chunks = split(url, '/')
+    print(vim.inspect(chunks))
+    local host = chunks[2]
+    if host ~= 'gitlab.com' then
+      error("Non Gitlab projects aren't supported")
+    end
+    local owner = chunks[3]
+    local project_git = chunks[4]
+    local project_name = string.match(project_git, '(.*)%.git')
+    print(host, owner, project_name)
+    return { host = host, owner = owner, name = project_name }
+  else
+    local _, rest = unpack(split(url, '@'))
+    local host, rest = unpack(split(rest, ':'))
+    local owner, project_git = unpack(split(rest, '/'))
+    local project_name = string.match(project_git, '(.*)%.git')
+    return { host = host, owner = owner, name = project_name }
+  end
+end
+
+function M.get_project()
+  local command = 'git config --get remote.origin.url'
+  local url = vim.fn.system(command)
+  return get_project_from_url(url)
 end
 
 return M
--- lua/glab-nvim/init.lua
+++ lua/glab-nvim/init.lua
@@ -5,19 +5,20 @@ local pickers = require "glab-nvim.telescope.pickers"
 local M = {}
 
 function M.pick_merge_requests()
-    local function on_result(output)
-        pickers.merge_request_picker(output)
-    end
-    local project = config.get_config().project
-    api.get_merge_requests(project.owner, project.name, on_result)
+  local function on_result(output)
+    pickers.merge_request_picker(output)
+  end
+
+  local project = config.get_project()
+  api.get_merge_requests(project.owner, project.name, on_result)
 end
 
 function M.setup(user_config)
-    for key, value in pairs(user_config) do
-        if value ~= nil then
-            config.config[key] = value
-        end
+  for key, value in pairs(user_config) do
+    if value ~= nil then
+      config.config[key] = value
     end
+  end
 end
 
 return M
