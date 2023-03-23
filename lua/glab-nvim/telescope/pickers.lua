f187c8a5800b8ea0dc8aa060d7d789a6af77160c
thib <thibault@seervision.com>
2023-03-23 14:52:40 +0400

    use project

--- lua/glab-nvim/telescope/pickers.lua
+++ lua/glab-nvim/telescope/pickers.lua
@@ -8,44 +8,44 @@ local glab_previewers = require "glab-nvim.telescope.previewers"
 local entry_maker = require "glab-nvim.telescope.entry_maker"
 local merge_request_buffer = require "glab-nvim.merge-request.merge_request_buffer"
 local api = require("glab-nvim.api.gitlab.api")
-local config = require("glab-nvim.config").get_config()
+local project = require("glab-nvim.config").get_project()
 
 local M = {}
 
 function M.merge_request_picker(merge_requests)
-    local picker_opts = {}
-    picker_opts.preview_title = "Preview"
-    picker_opts.prompt_title = ""
-    picker_opts.results_title = "Merge Requests"
-    pickers.new(
-        picker_opts,
-        {
-            finder = finders.new_table {
-                results = merge_requests,
-                entry_maker = entry_maker.gen_from_merge_request(4)
-            },
-            sorter = conf.generic_sorter(picker_opts),
-            previewer = glab_previewers.mr_preview(),
-            attach_mappings = function(_, map)
-                action_set.select:replace(
-                    function(prompt_bufnr, type)
-                        actions.close(prompt_bufnr)
-                        local entry = action_state.get_selected_entry()
-                        api.get_merge_request(
-                            config.project.owner,
-                            config.project.name,
-                            entry.merge_request.iid,
-                            function(merge_request)
-                                merge_request_buffer.create(merge_request)
-                            end
-                        )
-                    end
-                )
-                --map("i", "<c-b>", open())
-                return true
-            end
-        }
-    ):find()
+  local picker_opts = {}
+  picker_opts.preview_title = "Preview"
+  picker_opts.prompt_title = ""
+  picker_opts.results_title = "Merge Requests"
+  pickers.new(
+    picker_opts,
+    {
+      finder = finders.new_table {
+        results = merge_requests,
+        entry_maker = entry_maker.gen_from_merge_request(4)
+      },
+      sorter = conf.generic_sorter(picker_opts),
+      previewer = glab_previewers.mr_preview(),
+      attach_mappings = function(_, map)
+        action_set.select:replace(
+          function(prompt_bufnr, type)
+            actions.close(prompt_bufnr)
+            local entry = action_state.get_selected_entry()
+            api.get_merge_request(
+              project.owner,
+              project.name,
+              entry.merge_request.iid,
+              function(merge_request)
+                merge_request_buffer.create(merge_request)
+              end
+            )
+          end
+        )
+        --map("i", "<c-b>", open())
+        return true
+      end
+    }
+  ):find()
 end
 
 return M
