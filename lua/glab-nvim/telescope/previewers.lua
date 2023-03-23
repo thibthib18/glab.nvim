fe670589281dcc2c4c240fbd882101c278651ae0
thib <thibault@seervision.com>
2023-03-23 14:48:01 +0400

    fix

--- lua/glab-nvim/telescope/previewers.lua
+++ lua/glab-nvim/telescope/previewers.lua
@@ -15,7 +15,7 @@ function M.mr_preview()
       local bufnr = self.state.bufnr
       if self.state.bufname ~= entry.name or vim.api.nvim_buf_line_count(bufnr) == 1 then
         api.get_merge_request(
-          config.get_project().owner,
+          require('glab-nvim.config').get_project().owner,
           config.project.name,
           entry.merge_request.iid,
           function(merge_request)
