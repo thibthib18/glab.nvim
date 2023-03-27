f402800cd7f1bd91b2df435396e802a2d97ab7d5
thib <thibault@seervision.com>
2023-03-27 10:19:30 +0400

    format

--- lua/glab-nvim/config.lua
+++ lua/glab-nvim/config.lua
@@ -17,26 +17,32 @@ local function split(text, delimiter)
   return chunks
 end
 
+local function get_project_from_http(url)
+  local chunks = split(url, '/')
+  local host = chunks[2]
+  if host ~= 'gitlab.com' then
+    error("Non Gitlab projects aren't supported")
+  end
+  local owner = chunks[3]
+  local project_git = chunks[4]
+  local project_name = string.match(project_git, '(.*)%.git')
+  return { host = host, owner = owner, name = project_name }
+end
+
+local function get_project_from_ssh(url)
+  local _, rest = unpack(split(url, '@'))
+  local host, rest = unpack(split(rest, ':'))
+  local owner, project_git = unpack(split(rest, '/'))
+  local project_name = string.match(project_git, '(.*)%.git')
+  return { host = host, owner = owner, name = project_name }
+end
+
 local function get_project_from_url(url)
   local isHttp = string.match(url, 'https://') ~= nil
   if isHttp then
-    local chunks = split(url, '/')
-    print(vim.inspect(chunks))
-    local host = chunks[2]
-    if host ~= 'gitlab.com' then
-      error("Non Gitlab projects aren't supported")
-    end
-    local owner = chunks[3]
-    local project_git = chunks[4]
-    local project_name = string.match(project_git, '(.*)%.git')
-    print(host, owner, project_name)
-    return { host = host, owner = owner, name = project_name }
+    return get_project_from_http(url)
   else
-    local _, rest = unpack(split(url, '@'))
-    local host, rest = unpack(split(rest, ':'))
-    local owner, project_git = unpack(split(rest, '/'))
-    local project_name = string.match(project_git, '(.*)%.git')
-    return { host = host, owner = owner, name = project_name }
+    return get_project_from_ssh(url)
   end
 end
 
